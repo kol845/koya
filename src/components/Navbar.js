c2cfebdb34d91943c585e07ff42968d489f14cdb
Pebo Hamza <hamza.pebo@gmail.com>
2023-07-10 17:21:27 +0200

    Fix Navbar options

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
 import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
+import logo from "../img/branding/kurdistan-flag.png";
 
 const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
@@ -15,7 +15,7 @@ const Navbar = () => {
       <div className="container">
         <div className="navbar-brand">
           <Link to="/" className="navbar-item" title="Logo">
-            <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+            <img src={logo} alt="Kaldi" style={{ width: "30px" }} />
           </Link>
           {/* Hamburger menu */}
           <button
@@ -28,36 +28,26 @@ const Navbar = () => {
             <span />
           </button>
         </div>
-        <ul id="navMenu" className={` navbar-start has-text-centered navbar-menu ${isActive && "is-active"}`}>
-            {/* TODO: inline override of padding is a result of refactoring
+        <ul
+          id="navMenu"
+          className={` navbar-start has-text-centered navbar-menu ${
+            isActive && "is-active"
+          }`}
+        >
+          {/* TODO: inline override of padding is a result of refactoring
                 to a ul for accessibilty purposes, would like to see a css
                 re-write that makes this unneccesary.
              */}
-            <li className="navbar-item" style={{padding: "0px"}}>
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/products">
-              Products
+          <li className="navbar-item" style={{ padding: "0px" }}>
+            <Link className="navbar-item" to="/about">
+              About
             </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
+          </li>
+          <li className="navbar-item" style={{ padding: "0px" }}>
             <Link className="navbar-item" to="/blog">
               Blog
             </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/contact">
-              Contact
-            </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/contact/examples">
-              Form Examples
-            </Link>
-            </li>
+          </li>
           <li className="navbar-end has-text-centered">
             <a
               className="navbar-item"
